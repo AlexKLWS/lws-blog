bcd19338abe8ded5aa5d8042f03c6b5fb8ebbdcd
Alex Korzh <alexkorzh7@gmail.com>
2020-02-27 22:15:45 +0100

    Header WIP

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 import './Header.scss'
 
 const Header: React.FC = () => {
+  let history = useHistory()
+
+  const onTitlePress = () => {
+    history.push('/')
+  }
+
   return (
     <div className='Header-container'>
       <div className='Header-top-container'>
-        <h1 className='Header-title'>LONG WINTER SHADOWS</h1>
+        <h1 onClick={onTitlePress} className='Header-title'>
+          LONG WINTER SHADOWS
+        </h1>
+        <div className='Header-right-container'>
+          <span className='Header-subtitle'>Personal blog by Alex Korzh</span>
+          <div className='Header-portrait' />
+        </div>
       </div>
       <div className='Sections-list'>
         <Link to='/life'>LIFE</Link>
