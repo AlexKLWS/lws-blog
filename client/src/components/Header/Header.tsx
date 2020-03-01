d2aab323ac2bbabfb980fa0b4a09e959a24b0d2b
Alex Korzh <alexkorzh7@gmail.com>
2020-03-01 15:43:08 +0100

    Header inital stateful setup

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,31 +1,47 @@
 import React from 'react'
+import Img from 'react-image'
 import { Link, useHistory } from 'react-router-dom'
 
 import './Header.scss'
+import routes from 'consts/routes'
 
 const Header: React.FC = () => {
-  let history = useHistory()
+  const history = useHistory()
 
-  const onTitlePress = () => {
-    history.push('/')
+  const getLinkStyle = (route: string) => {
+    const currentRoute = history.location.pathname
+    return route === currentRoute ? 'Sections-item active' : 'Sections-item'
   }
 
   return (
     <div className='Header-container'>
       <div className='Header-top-container'>
-        <h1 onClick={onTitlePress} className='Header-title'>
+        <Link to={routes.home} className='Header-title'>
           LONG WINTER SHADOWS
-        </h1>
+        </Link>
         <div className='Header-right-container'>
-          <span className='Header-subtitle'>Personal blog by Alex Korzh</span>
-          <div className='Header-portrait' />
+          <span className='Header-subtitle'>Personal blog by</span>
+          <Link to={routes.contact} className='Header-subtitle link'>
+            Alex Korzh
+          </Link>
+          <div className='Header-portrait'>
+            <Img src={process.env.PUBLIC_URL + '/square_1.jpg'} width='auto' height='100%' />
+          </div>
         </div>
       </div>
       <div className='Sections-list'>
-        <Link to='/life'>LIFE</Link>
-        <Link to='/code'>CODE</Link>
-        <Link to='/guides'>GUIDES</Link>
-        <Link to='/projects'>PROJECTS</Link>
+        <Link to={routes.life} className={getLinkStyle(routes.life)}>
+          Life
+        </Link>
+        <Link to={routes.code} className={getLinkStyle(routes.code)}>
+          Code
+        </Link>
+        <Link to={routes.guides} className={getLinkStyle(routes.guides)}>
+          Guides
+        </Link>
+        <Link to={routes.projects} className={getLinkStyle(routes.projects)}>
+          Projects
+        </Link>
       </div>
     </div>
   )
---
+++ client/src/consts/routes.ts
@@ -0,0 +1,17 @@
+const home = '/'
+const life = '/life'
+const code = '/code'
+const guides = '/guides'
+const projects = '/projects'
+const contact = '/contact'
+const login = '/login'
+
+export default {
+  home,
+  life,
+  code,
+  guides,
+  projects,
+  contact,
+  login,
+}
