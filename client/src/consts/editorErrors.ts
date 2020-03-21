82ac7ab0546165a9ab649c1fb95fe97984742160
Alex Korzh <alexkorzh7@gmail.com>
2020-03-21 21:59:34 +0100

    Add editor errors object

---
+++ client/src/consts/editorErrors.ts
@@ -0,0 +1,20 @@
+const noArticleName = {
+  id: 'no-article-name',
+  description: 'No article title specified!',
+}
+
+const noArticleSubtitle = {
+  id: 'no-article-subtitle',
+  description: 'No article subtitle specified!',
+}
+
+const noArticleText = {
+  id: 'no-article-text',
+  description: 'No article content!',
+}
+
+export default {
+  noArticleName,
+  noArticleSubtitle,
+  noArticleText,
+}
