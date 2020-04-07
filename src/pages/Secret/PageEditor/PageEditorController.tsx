edeec00bcb8e03762055417f3ebe84194703a379
Alex Korzh <alexkorzh7@gmail.com>
2020-04-08 01:08:52 +0200

    Style and clean up page editor

--- src/pages/Secret/PageEditor/PageEditorController.tsx
+++ src/pages/Secret/PageEditor/PageEditorController.tsx
@@ -1,30 +1,46 @@
 import React, { useState } from 'react'
-import PagePreviewEditorWidget from 'components/PagePreviewEditorWidget'
+import Loadable from 'react-loadable'
 
-const PageEditorController = () => {
-  const [pageName, setPageName] = useState('')
-  const [pageSubtitle, setPageSubtitle] = useState('')
-  const [pageIcon, setPageIcon] = useState<File | null>(null)
-  const [pageIconWidth, setPageIconWidth] = useState('')
-  const [pageIconHeight, setPageIconHeight] = useState('')
+import { EditorError } from 'types/editor'
+import pageEditorErrors from 'consts/pageEditorErrors'
+import { usePagePostFacade } from 'services/facades/pagePostFacade'
+import { RouteProps } from 'react-router-dom'
+
+const LoadablePageEditorView = Loadable({
+  loader: () => import('./PageEditorView'),
+  loading: () => {
+    return <div>LOADING</div>
+  },
+})
+
+const PageEditorController: React.FC<RouteProps> = (props: RouteProps) => {
+  const [currentSubmitErrors, setSubmitErrors] = useState<EditorError[]>([])
+
+  const [postPage] = usePagePostFacade()
+
+  const performDataCheck = (pageName: string, pageSubtitle: string, pageIcon: File | null, pageURL: string) => {
+    const errors: EditorError[] = []
+    if (!pageName) {
+      errors.push(pageEditorErrors.noPageName)
+    }
+    if (!pageSubtitle) {
+      errors.push(pageEditorErrors.noPageSubtitle)
+    }
+    if (!pageIcon) {
+      errors.push(pageEditorErrors.noPageIcon)
+    }
+    if (!pageURL) {
+      errors.push(pageEditorErrors.noPageURL)
+    }
+    setSubmitErrors(errors)
+  }
 
   return (
-    <div>
-      <h1 className='App-title'>Page Editor</h1>
-      <PagePreviewEditorWidget
-        name={pageName}
-        subtitle={pageSubtitle}
-        icon={pageIcon}
-        iconWidth={pageIconWidth}
-        iconHeight={pageIconHeight}
-        setName={setPageName}
-        setSubtitle={setPageSubtitle}
-        setIcon={setPageIcon}
-        setIconWidth={setPageIconWidth}
-        setIconHeight={setPageIconHeight}
-      />
-      <input placeholder='URL' className='App-input' onChange={(event: React.ChangeEvent<HTMLInputElement>) => {}} />
-    </div>
+    <LoadablePageEditorView
+      submitErrors={currentSubmitErrors}
+      performDataCheck={performDataCheck}
+      submitData={postPage}
+    />
   )
 }
 
---
+++ src/pages/Secret/PageEditor/PageEditorView.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+
+import './PageEditor.scss'
+import PagePreviewEditorWidget from 'components/PagePreviewEditorWidget'
+import { EditorError } from 'types/editor'
+import SubmitModal from 'components/Secret/SubmitModal'
+
+interface Props {
+  submitData: (
+    pageName: string,
+    pageSubtitle: string,
+    pageIcon: File,
+    pageIconWidth: string,
+    pageIconHeight: string,
+    pageURL: string,
+  ) => void
+  performDataCheck: (pageName: string, pageSubtitle: string, pageIcon: File | null, pageURL: string) => void
+  submitErrors: EditorError[]
+}
+
+const PageEditorView = (props: Props) => {
+  const [pageName, setPageName] = useState('')
+  const [pageSubtitle, setPageSubtitle] = useState('')
+  const [pageURL, setPageURL] = useState('')
+  const [pageIcon, setPageIcon] = useState<File | null>(null)
+  const [pageIconWidth, setPageIconWidth] = useState('')
+  const [pageIconHeight, setPageIconHeight] = useState('')
+
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const onSubmit = () => {
+    if (!pageIcon) {
+      return
+    }
+    props.submitData(pageName, pageSubtitle, pageIcon, pageIconWidth, pageIconHeight, pageURL)
+    setModalIsOpen(false)
+  }
+
+  const onSubmitButtonClick = () => {
+    props.performDataCheck(pageName, pageSubtitle, pageIcon, pageURL)
+    setModalIsOpen(true)
+  }
+
+  const onURLInputValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPageURL(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1 className='App-title'>Page Editor</h1>
+      <PagePreviewEditorWidget
+        name={pageName}
+        subtitle={pageSubtitle}
+        icon={pageIcon}
+        iconWidth={pageIconWidth}
+        iconHeight={pageIconHeight}
+        setName={setPageName}
+        setSubtitle={setPageSubtitle}
+        setIcon={setPageIcon}
+        setIconWidth={setPageIconWidth}
+        setIconHeight={setPageIconHeight}
+      />
+      <div className='PE-URL-container'>
+        <input placeholder='URL' className='App-input' onChange={onURLInputValueChange} />
+      </div>
+      <div className='PE-button-container'>
+        <input className='App-button' onClick={onSubmitButtonClick} type={'submit'} value={'Submit'} />
+      </div>
+      <SubmitModal
+        modalIsOpen={modalIsOpen}
+        submitErrors={props.submitErrors}
+        setModalIsOpen={setModalIsOpen}
+        onSubmit={onSubmit}
+      />
+    </div>
+  )
+}
+
+export default PageEditorView
