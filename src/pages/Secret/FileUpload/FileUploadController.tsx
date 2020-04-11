14b2584490418f58baca33ef2de32733105cede5
Alex Korzh <alexkorzh7@gmail.com>
2020-04-11 17:06:32 +0200

    Setup file-upload page

--- src/pages/Secret/FileUpload/FileUploadController.tsx
+++ src/pages/Secret/FileUpload/FileUploadController.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import FileUploadWidget from 'components/FileUploadWidget'
+import React, { useState } from 'react'
+
+import FileUploadView from './FileUploadView'
+import { EditorError } from 'types/editor'
 
 const FileUploadController = () => {
-  return (
-    <div>
-      <h1 className='App-title'>Upload Files</h1>
-      <FileUploadWidget />
-    </div>
-  )
+  const [currentSubmitErrors, setSubmitErrors] = useState<EditorError[]>([])
+
+  return <FileUploadView submitErrors={currentSubmitErrors} />
 }
 
 export default FileUploadController
