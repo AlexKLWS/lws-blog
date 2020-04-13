b7a5c595d530b8db92621662c241b284988fd3f8
Alex Korzh <alexkorzh7@gmail.com>
2020-04-13 18:24:05 +0200

    Tweak and fix file upload service WIP

--- src/services/fileUpload.ts
+++ src/services/fileUpload.ts
@@ -23,9 +23,9 @@ export class FileUploadService implements IFileUploadService {
   }
 
   private createMetadata(folderData: FolderData[]): UploadMetaDataBody {
-    const folderMetadatas: FileMetaData[] = []
+    let folderMetadatas: FileMetaData[] = []
     for (const folder of folderData) {
-      const files: FileMetaData[] = folder.files
+      const files: FileMetaData[] = !!folder.files
         ? folder.files.map((file) => {
             return {
               folder: folder.folder,
@@ -34,21 +34,39 @@ export class FileUploadService implements IFileUploadService {
             }
           })
         : []
-      folderMetadatas.concat(files)
+      folderMetadatas = folderMetadatas.concat(files)
     }
+    console.log('FOLDER-METADATAS: ', folderMetadatas)
     return {
       metaData: folderMetadatas,
     }
   }
 
+  private createFormdata(folderData: FolderData[]): FormData[] {
+    const formDatas: FormData[] = []
+    for (const folder of folderData) {
+      if (!folder.files) {
+        continue
+      }
+      for (const file of folder.files) {
+        const data = new FormData()
+        data.append('referenceId', file.referenceId!)
+        data.append('file', file.file!)
+        formDatas.push(data)
+      }
+    }
+    return formDatas
+  }
+
   public async uploadFiles(folderData: FolderData[], uploadProgressCallback: (progressEvent: any) => void) {
+    console.log('FOLDER-DATA: ', folderData)
     // Not very efficient, but the code is cleaner
     // TODO: Think how to refactor
     this.assignReferenceIdsToFiles(folderData)
     const data = this.createMetadata(folderData)
 
     const metaDataRequest: AxiosRequestConfig = {
-      method: 'POST',
+      method: 'PUT',
       onUploadProgress: uploadProgressCallback,
       url: `${apiEndpoint}/files/metadata`,
       withCredentials: true,
@@ -56,10 +74,31 @@ export class FileUploadService implements IFileUploadService {
     }
 
     try {
-      const metaDataResponse = await axios(metaDataRequest)
-      const metaDataResponseStatus = metaDataResponse.status
-      console.log('METADATA-UPLOAD-STATUS: ', metaDataResponseStatus)
+      console.log('SENDING-DATA: ', data)
+      const response = await axios(metaDataRequest)
+      console.log('RESPONSE-CODE: ', response.status)
     } catch (error) {}
+
+    const formData = this.createFormdata(folderData)
+    await Promise.all(
+      formData.map(async (data) => {
+        const fileDataRequest: AxiosRequestConfig = {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          method: 'PUT',
+          onUploadProgress: uploadProgressCallback,
+          url: `${apiEndpoint}/files`,
+          withCredentials: true,
+          data,
+        }
+        const fileURLResponse = await axios(fileDataRequest)
+        const fileURL = await fileURLResponse.data()
+        console.log('FILE-UPLOADED-WITH-URL: ', fileURL)
+      }),
+    ).catch((error) => {
+      console.log('FILE-UPLOAD-ERROR: ', error)
+    })
   }
 }
 
