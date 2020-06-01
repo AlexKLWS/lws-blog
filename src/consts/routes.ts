8ab6aa375f6c225f68f55da9f7259ff41dff875a
Alex Korzh <alexkorzh7@gmail.com>
2020-06-01 19:54:34 +0200

    Add article routes

--- src/consts/routes.ts
+++ src/consts/routes.ts
@@ -1,8 +1,13 @@
 const home = '/'
+const miscArticle = '/:id'
 const life = '/life'
+const lifeArticle = '/life/:id'
 const code = '/code'
+const codeArticle = '/code/:id'
 const guides = '/guides'
+const guidesArticle = '/guides/:id'
 const projects = '/projects'
+const projectsArticle = '/projects/:id'
 const contact = '/contact'
 const login = '/login'
 const secret = {
@@ -14,10 +19,15 @@ const secret = {
 
 export default {
   home,
+  miscArticle,
   life,
+  lifeArticle,
   code,
+  codeArticle,
   guides,
+  guidesArticle,
   projects,
+  projectsArticle,
   contact,
   login,
   secret,
