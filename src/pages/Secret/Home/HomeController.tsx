20b7e5da1ec95822b38c84cf3764b6ae091ba0d1
Alex Korzh <alexkorzh7@gmail.com>
2020-07-05 20:04:43 +0200

    Add secret page materials list fetch

--- src/pages/Secret/Home/HomeController.tsx
+++ src/pages/Secret/Home/HomeController.tsx
@@ -1,10 +1,51 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
 import HomeView from './HomeView'
 import routes from 'consts/routes'
+import { useMaterialPreviewsProvider } from 'facades/materialPreviewsFetchFacade'
+import { resolveCategoryFromPathname } from 'helpers/resolveCategory'
+import { page } from 'consts/query'
+import { PreviewMaterial } from 'types/materials'
 
 const HomeController: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
+  const { materialPreviews, pagesCount, fetchMaterialPreviews } = useMaterialPreviewsProvider()
+  const [currentPage, setCurrentPage] = useState(1)
+
+  useEffect(() => {
+    const category = resolveCategoryFromPathname(props.location as any)
+    const pageFromQuery = new URLSearchParams(props.location?.search).get(page) || 1
+    fetchMaterialPreviews(category, pageFromQuery)
+    if (pageFromQuery !== currentPage) {
+      setCurrentPage(pageFromQuery as number)
+    }
+  }, [props.location])
+
+  const navigateToNextPage = () => {
+    let newPage = currentPage + 1
+    if (newPage > pagesCount) {
+      newPage = pagesCount
+    }
+    props.history.push(`${props.location.pathname}?${page}=${newPage}`)
+    setCurrentPage(newPage)
+  }
+
+  const navigateToPrevPage = () => {
+    let newPage = currentPage - 1
+    if (newPage < 1) {
+      newPage = 1
+    }
+    props.history.push(`${props.location.pathname}?${page}=${newPage}`)
+    setCurrentPage(newPage)
+  }
+
+  const onPreviewItemPress = (previewMaterial: PreviewMaterial) => {
+    if (previewMaterial.pageURL) {
+      props.history.push(`${routes.secret.pageEditor}/${previewMaterial.referenceId}`)
+    } else {
+      props.history.push(`${routes.secret.editor}/${previewMaterial.referenceId}`)
+    }
+  }
   const dropdownItems = useMemo(
     () => [
       {
@@ -16,7 +57,7 @@ const HomeController: React.FC<RouteComponentProps> = (props: RouteComponentProp
       {
         label: 'Add page',
         callback: () => {
-          props.history.push(routes.secret.addPage)
+          props.history.push(routes.secret.pageEditor)
         },
       },
       {
@@ -29,7 +70,17 @@ const HomeController: React.FC<RouteComponentProps> = (props: RouteComponentProp
     [],
   )
 
-  return <HomeView dropdownItems={dropdownItems} />
+  return (
+    <HomeView
+      materialPreviews={materialPreviews}
+      navigateToPrevPage={navigateToPrevPage}
+      navigateToNextPage={navigateToNextPage}
+      currentPage={currentPage}
+      pagesCount={pagesCount}
+      onPreviewItemPress={onPreviewItemPress}
+      dropdownItems={dropdownItems}
+    />
+  )
 }
 
 export default HomeController
--- src/pages/Secret/Home/HomeView.tsx
+++ src/pages/Secret/Home/HomeView.tsx
@@ -2,15 +2,102 @@ import React from 'react'
 
 import Dropdown from 'components/Dropdown/Dropdown'
 import { DropdownItem } from 'types/dropdown'
+import { PreviewMaterial } from 'types/materials'
+import { ReactComponent as Arrow } from 'assets/icons/Arrow.svg'
+import InlineIcon from 'components/InlineIcon'
 
 interface Props {
   dropdownItems: DropdownItem[]
+  materialPreviews: PreviewMaterial[]
+  currentPage: number
+  pagesCount: number
+  navigateToNextPage: () => void
+  navigateToPrevPage: () => void
+  onPreviewItemPress: (previewMaterial: PreviewMaterial) => void
 }
 
 const HomeView: React.FC<Props> = (props: Props) => {
+  const renderPreviewsItems = (previewMaterial: PreviewMaterial) => {
+    return (
+      <div key={previewMaterial.referenceId} className={'Material-preview-item-container'}>
+        <div
+          tabIndex={0}
+          className={'Material-preview-item'}
+          onClick={() => {
+            props.onPreviewItemPress(previewMaterial)
+          }}
+          onKeyPressCapture={() => {
+            props.onPreviewItemPress(previewMaterial)
+          }}
+        >
+          <div className={'Material-preview-icon-container'}>
+            <InlineIcon svg={previewMaterial.icon.data} />
+          </div>
+          <div>
+            <p className={'Material-preview-items-title'}>{previewMaterial.name}</p>
+            <p className={'Material-preview-items-subtitle'}>{previewMaterial.subtitle}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const renderPreviewsList = () => {
+    let index = 0
+    const previewItemsRows = []
+
+    while (index < props.materialPreviews.length) {
+      const rowItems = []
+      rowItems.push(props.materialPreviews[index])
+      index++
+      if (index < props.materialPreviews.length) {
+        rowItems.push(props.materialPreviews[index])
+        index++
+      }
+      const row = (
+        <div key={`${index}`} className={'Material-preview-items-row'}>
+          {renderPreviewsItems(rowItems[0])}
+          {!!rowItems[1] ? (
+            <>
+              <div style={{ width: '16px' }} />
+              {renderPreviewsItems(rowItems[1])}
+            </>
+          ) : (
+            <>
+              <div style={{ width: '16px' }} />
+              <div style={{ display: 'flex', flex: '1 1 100%' }} />
+            </>
+          )}
+        </div>
+      )
+      previewItemsRows.push(row)
+    }
+    return previewItemsRows
+  }
+
+  const renderPageControls = () => {
+    return (
+      <div className='Pagination-controls-container'>
+        <div style={{ display: 'flex' }}>
+          <button className='Arrow-container' onClick={props.navigateToPrevPage}>
+            <Arrow />
+          </button>
+          <p className='Page-index'>{`${props.currentPage}/${props.pagesCount}`}</p>
+          <button className='Arrow-container' onClick={props.navigateToNextPage}>
+            <Arrow className={'Arrow-right'} />
+          </button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Dropdown dropdownTriggerText={'Add'} items={props.dropdownItems} />
+      <div className='Home-container'>
+        {renderPreviewsList()}
+        {props.pagesCount > 1 && renderPageControls()}
+      </div>
     </div>
   )
 }
