da04c638d0bf6d74a7377d5308c884c86e7a64ba
Alex Korzh <alexkorzh7@gmail.com>
2020-07-12 21:32:42 +0200

    Add page for non-existing url

---
+++ src/pages/EmptyPage/EmptyPageController.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import EmptyPageView from './EmptyPageView'
+
+const EmptyPageController: React.FC = () => {
+  return <EmptyPageView />
+}
+
+export default EmptyPageController
