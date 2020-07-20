95ec077591e3d423a0f2280d03e6802a438e9d9b
Alex Korzh <alexkorzh7@gmail.com>
2020-07-20 22:27:30 +0200

    Setup guide pin component

--- src/components/GuidePin/GuidePin.tsx
+++ src/components/GuidePin/GuidePin.tsx
@@ -3,32 +3,30 @@ import React from 'react'
 import './GuidePin.scss'
 
 import { ReactComponent as MapPin } from 'assets/icons/MapPin.svg'
-import { ReactComponent as Cafe } from 'assets/icons/Cafe.svg'
-import { ReactComponent as Bar } from 'assets/icons/Bar.svg'
-import { ReactComponent as Restaurant } from 'assets/icons/Restaurant.svg'
-import { ReactComponent as GenericIcon } from 'assets/icons/Generic.svg'
-import { PinType, GuidePinData } from 'types/guide'
+import { PinType } from 'types/guide'
+import GuideItemIcon from 'components/GuideItemIcon/GuideItemIcon'
 
-const GuidePin: React.FC<GuidePinData> = (props: GuidePinData) => {
-  const pinIconStyle = { width: '20px', height: '20px' }
+type Props = {
+  type: PinType
+  lat: number
+  lng: number
+  onPinPress: () => void
+}
 
-  const getPinIcon = () => {
-    switch (props.type) {
-      case PinType.BAR:
-        return <Bar style={pinIconStyle} />
-      case PinType.CAFE:
-        return <Cafe style={pinIconStyle} />
-      case PinType.RESTAURANT:
-        return <Restaurant style={pinIconStyle} />
-      default:
-        return <GenericIcon style={pinIconStyle} />
-    }
-  }
-
+const GuidePin: React.FC<Props> = (props: Props) => {
   return (
     <div className={'Guide-pin-container'}>
-      <div className={'Guide-pin-icon-container'}>{getPinIcon()}</div>
-      <MapPin style={{ width: '50px', height: '50px' }} />
+      <div className={'Guide-pin-icon-container'}>
+        <GuideItemIcon type={props.type} />
+      </div>
+      <MapPin style={{ width: '35px', height: '50px' }} />
+      <input
+        className={'Guide-pin-button'}
+        type={'button'}
+        onClick={() => {
+          props.onPinPress()
+        }}
+      />
     </div>
   )
 }
