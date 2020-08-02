47107b1dbe139f50e892bf4c86ed2a8370420223
Alex Korzh <alexkorzh7@gmail.com>
2020-08-02 19:38:44 +0200

    Refactor page preview editor widget to use new service

--- src/components/PagePreviewEditorWidget/PagePreviewEditorWidget.tsx
+++ src/components/PagePreviewEditorWidget/PagePreviewEditorWidget.tsx
@@ -1,110 +1,139 @@
-import React, { useState, useMemo } from 'react'
+import React from 'react'
 
 import './PagePreviewEditorWidget.scss'
 import { Category } from 'types/materials'
-import { DropdownItem } from 'types/dropdown'
-import Dropdown from 'components/Dropdown/Dropdown'
+import { IMaterialDataService } from 'services/materialData'
+import InputDataController from 'components/MaterialDataFormItems/Input/InputDataController'
+import EnumDropdown from 'components/Dropdowns/EnumDropdown/EnumDropdown'
 
-interface Props {
-  name: string
-  subtitle: string
-  icon: File | string | null
-  iconWidth: string
-  iconHeight: string
-  category: Category
-  setName: (value: string) => void
-  setSubtitle: (value: string) => void
-  setIcon: (value: File | string | null) => void
-  setIconWidth: (value: string) => void
-  setIconHeight: (value: string) => void
-  setCategory?: (value: Category) => void
+type Props = {
+  serviceInstance: IMaterialDataService
+  categoryToggleDisabled?: boolean
 }
 
 const PagePreviewEditorWidget: React.FC<Props> = (props: Props) => {
-  const onNameInputValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.setName(event.target.value)
-  }
-
-  const onSubtitleInputValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.setSubtitle(event.target.value)
-  }
-
-  const onArticleIconWidthValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.setIconWidth(event.target.value)
-  }
-
-  const onArticleIconHeightValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.setIconHeight(event.target.value)
-  }
-
-  const onIconFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.files || !event.target.files[0]) {
-      return
-    }
-    props.setIcon(event.target.files[0])
-  }
-
-  const dropdownItems: DropdownItem[] = useMemo(() => {
-    const items: DropdownItem[] = []
-    for (let item in Category) {
-      const index = Number(item)
-      if (typeof index === 'number' && !isNaN(index)) {
-        const category = Category[index]
-        items.push({
-          label: category,
-          callback: () => {
-            if (props.setCategory) {
-              props.setCategory(index)
-            }
-          },
-        })
-      }
-    }
-    return items
-  }, [])
-
-  const getIconButtonText = () => {
-    if (!props.icon) {
+  const getIconButtonText = (value: File | string | null) => {
+    if (!value) {
       return 'Add icon'
     }
-    if (!(props.icon as File).name) {
+    if (!(value as File).name) {
       return 'Change icon'
     }
-    return (props.icon as File).name
+    return (value as File).name
   }
 
   return (
     <div className='PPEW-input-container'>
-      <input className='PPEW-input' placeholder='Title' value={props.name} onChange={onNameInputValueChange} />
-      <input
-        className='PPEW-input'
-        placeholder='Subtitle'
-        value={props.subtitle}
-        onChange={onSubtitleInputValueChange}
+      <InputDataController
+        serviceInstance={props.serviceInstance}
+        path={'name'}
+        render={({ value, setValue }) => {
+          return (
+            <input
+              className='PPEW-input'
+              placeholder='Title'
+              value={value}
+              onChange={(event) => {
+                setValue(event.target.value)
+              }}
+            />
+          )
+        }}
       />
-      <input type='file' className='App-file-input' id='articleIcon' onChange={onIconFileChange} accept='.svg' />
-      <label className='App-button' htmlFor='articleIcon'>
-        {getIconButtonText()}
-      </label>
-      {props.icon && (
-        <div className='PPEW-icon-row'>
-          <input
-            className='PPEW-icon-dimensions-input'
-            placeholder='Width'
-            value={props.iconWidth}
-            onChange={onArticleIconWidthValueChange}
-          />
-          <input
-            className='PPEW-icon-dimensions-input'
-            placeholder='Height'
-            value={props.iconHeight}
-            onChange={onArticleIconHeightValueChange}
-          />
-        </div>
-      )}
+      <InputDataController
+        serviceInstance={props.serviceInstance}
+        path={'subtitle'}
+        render={({ value, setValue }) => {
+          return (
+            <input
+              className='PPEW-input'
+              placeholder='Subtitle'
+              value={value}
+              onChange={(event) => {
+                setValue(event.target.value)
+              }}
+            />
+          )
+        }}
+      />
+      <InputDataController
+        serviceInstance={props.serviceInstance}
+        path={'subtitle'}
+        render={({ value, setValue }) => {
+          const onIconFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            if (!event.target.files || !event.target.files[0]) {
+              return
+            }
+            setValue(event.target.files[0])
+          }
+          return (
+            <>
+              <input
+                type='file'
+                className='App-file-input'
+                id='articleIcon'
+                onChange={onIconFileChange}
+                accept='.svg'
+              />
+              <label className='App-button' htmlFor='articleIcon'>
+                {getIconButtonText(value)}
+              </label>
+              {value && (
+                <div className='PPEW-icon-row'>
+                  <InputDataController
+                    serviceInstance={props.serviceInstance}
+                    path={'icon.width'}
+                    render={({ value, setValue }) => {
+                      return (
+                        <input
+                          className='PPEW-icon-dimensions-input'
+                          placeholder='Width'
+                          value={value}
+                          onChange={(event) => {
+                            setValue(event.target.value)
+                          }}
+                        />
+                      )
+                    }}
+                  />
+                  <InputDataController
+                    serviceInstance={props.serviceInstance}
+                    path={'icon.height'}
+                    render={({ value, setValue }) => {
+                      return (
+                        <input
+                          className='PPEW-icon-dimensions-input'
+                          placeholder='Height'
+                          value={value}
+                          onChange={(event) => {
+                            setValue(event.target.value)
+                          }}
+                        />
+                      )
+                    }}
+                  />
+                </div>
+              )}
+            </>
+          )
+        }}
+      />
       <div className='PPEW-dropdown-container'>
         <span className='PPEW-dropdown-label'>Category:</span>
-        <Dropdown dropdownTriggerText={Category[props.category]} items={dropdownItems} disabled={!props.setCategory} />
+        <InputDataController
+          serviceInstance={props.serviceInstance}
+          path={'category'}
+          render={({ value, setValue }) => {
+            return (
+              <EnumDropdown
+                sourceEnum={Category as any}
+                value={value}
+                setValue={setValue}
+                disabled={!props.categoryToggleDisabled}
+              />
+            )
+          }}
+        />
       </div>
     </div>
   )
