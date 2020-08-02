77df0b50c1a598f08ca7a7d6fa98f57389308254
Alex Korzh <alexkorzh7@gmail.com>
2020-08-02 19:15:04 +0200

    Rearrange dropdowns

---
+++ src/components/Dropdowns/EnumDropdown/EnumDropdown.tsx
@@ -0,0 +1,35 @@
+import React, { useMemo } from 'react'
+import { DropdownItem } from 'types/dropdown'
+import Dropdown from '../Dropdown/Dropdown'
+
+type Props = {
+  sourceEnum: { [key: string]: string }
+  value: number
+  setValue: (index: number) => void
+  disabled?: boolean
+}
+
+const EnumDropdown = (props: Props) => {
+  const dropdownItems: DropdownItem[] = useMemo(() => {
+    const items: DropdownItem[] = []
+    for (let item in props.sourceEnum) {
+      const index = Number(item)
+      if (typeof index === 'number' && !isNaN(index)) {
+        const category = props.sourceEnum[index]
+        items.push({
+          label: category,
+          callback: () => {
+            props.setValue(index)
+          },
+        })
+      }
+    }
+    return items
+  }, [])
+
+  return (
+    <Dropdown dropdownTriggerText={props.sourceEnum[props.value]} items={dropdownItems} disabled={props.disabled} />
+  )
+}
+
+export default EnumDropdown
