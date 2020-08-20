6dc50deb338c6562ac2fe38bc252beb966be0dbb
Alex Korzh <alexkorzh7@gmail.com>
2020-08-20 21:18:41 +0200

    Rearrange errors consts and add guide data verifier

--- src/consts/verifiers.ts
+++ src/consts/verifiers.ts
@@ -1,25 +1,61 @@
 import { MaterialDataObjectVerifier, VerifiedPropertyType } from 'types/verifier'
-import pageEditorErrors from './pageEditorErrors'
-import editorErrors from './editorErrors'
+import { materialErrors, editorErrors, pageEditorErrors, guideEditorErrors } from './errors'
 
 export const ARTICLE_DATA_VERIFIER: MaterialDataObjectVerifier = {
-  name: { error: editorErrors.noArticleName, type: VerifiedPropertyType.PRIMITIVE },
-  subtitle: { error: editorErrors.noArticleSubtitle, type: VerifiedPropertyType.PRIMITIVE },
+  name: { error: materialErrors.noName, type: VerifiedPropertyType.PRIMITIVE },
+  subtitle: { error: materialErrors.noSubtitle, type: VerifiedPropertyType.PRIMITIVE },
   icon: {
-    error: editorErrors.noArticleIcon,
+    error: materialErrors.noIcon,
     type: VerifiedPropertyType.OBJECT,
-    innerObject: { data: { error: editorErrors.noArticleIcon, type: VerifiedPropertyType.PRIMITIVE } },
+    innerObject: { data: { error: materialErrors.noIcon, type: VerifiedPropertyType.PRIMITIVE } },
   },
   articleText: { error: editorErrors.noArticleText, type: VerifiedPropertyType.PRIMITIVE },
 }
 
 export const PAGE_DATA_VERIFIER: MaterialDataObjectVerifier = {
-  name: { error: pageEditorErrors.noPageName, type: VerifiedPropertyType.PRIMITIVE },
-  subtitle: { error: pageEditorErrors.noPageSubtitle, type: VerifiedPropertyType.PRIMITIVE },
+  name: { error: materialErrors.noName, type: VerifiedPropertyType.PRIMITIVE },
+  subtitle: { error: materialErrors.noSubtitle, type: VerifiedPropertyType.PRIMITIVE },
   icon: {
-    error: pageEditorErrors.noPageIcon,
+    error: materialErrors.noIcon,
     type: VerifiedPropertyType.OBJECT,
-    innerObject: { data: { error: pageEditorErrors.noPageIcon, type: VerifiedPropertyType.PRIMITIVE } },
+    innerObject: { data: { error: materialErrors.noIcon, type: VerifiedPropertyType.PRIMITIVE } },
   },
   pageURL: { error: pageEditorErrors.noPageURL, type: VerifiedPropertyType.PRIMITIVE },
+}
+
+export const LOCATION_DATA_VERIFIER: MaterialDataObjectVerifier = {
+  lat: { error: guideEditorErrors.noLatitude, type: VerifiedPropertyType.PRIMITIVE },
+  lng: { error: guideEditorErrors.noLongitude, type: VerifiedPropertyType.PRIMITIVE },
+}
+
+export const GUIDE_DATA_VERIFIER: MaterialDataObjectVerifier = {
+  name: { error: materialErrors.noName, type: VerifiedPropertyType.PRIMITIVE },
+  subtitle: { error: materialErrors.noSubtitle, type: VerifiedPropertyType.PRIMITIVE },
+  icon: {
+    error: materialErrors.noIcon,
+    type: VerifiedPropertyType.OBJECT,
+    innerObject: { data: { error: materialErrors.noIcon, type: VerifiedPropertyType.PRIMITIVE } },
+  },
+  defaultZoom: { error: guideEditorErrors.noDefaultZoom, type: VerifiedPropertyType.PRIMITIVE },
+  defaultCenter: {
+    error: guideEditorErrors.noDefaultCenter,
+    type: VerifiedPropertyType.OBJECT,
+    innerObject: LOCATION_DATA_VERIFIER,
+  },
+  locations: {
+    error: guideEditorErrors.noLocations,
+    type: VerifiedPropertyType.OBJECTARRAY,
+    innerObject: {
+      type: { error: guideEditorErrors.noLocationType, type: VerifiedPropertyType.PRIMITIVE },
+      coordinates: {
+        error: guideEditorErrors.noLocationCoordinates,
+        type: VerifiedPropertyType.OBJECT,
+        innerObject: LOCATION_DATA_VERIFIER,
+      },
+      address: { error: guideEditorErrors.noLocationAddress, type: VerifiedPropertyType.PRIMITIVE },
+      title: { error: guideEditorErrors.noLocationTitle, type: VerifiedPropertyType.PRIMITIVE },
+      description: { error: guideEditorErrors.noLocationDescription, type: VerifiedPropertyType.PRIMITIVE },
+      imageUrl: { error: guideEditorErrors.noLocationImageUrl, type: VerifiedPropertyType.PRIMITIVE },
+    },
+  },
 }
