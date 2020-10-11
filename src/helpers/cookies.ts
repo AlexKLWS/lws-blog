71da25839df770893f8aa722083e371bddb5b180
Alex Korzh <alexkorzh7@gmail.com>
2020-10-12 01:11:28 +0200

    Add cookie get helper

--- src/helpers/cookies.ts
+++ src/helpers/cookies.ts
@@ -4,3 +4,19 @@ export function setCookie(name: string, value: string, daysToExpire: number) {
   const expires = '; expires=' + date.toUTCString()
   document.cookie = name + '=' + value + expires + '; path=/'
 }
+
+export function getCookie(cookieName: string) {
+  var name = cookieName + '='
+  var decodedCookie = decodeURIComponent(document.cookie)
+  var ca = decodedCookie.split(';')
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ''
+}
