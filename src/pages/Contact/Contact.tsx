8915063163771378b18c8a7d0d4d80099c5d4df8
Alex Korzh <alexkorzh7@gmail.com>
2020-11-20 17:31:38 +0100

    Update contacts

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -9,20 +9,26 @@ const Contact: React.FC = () => {
     <div className='Contact-container'>
       <h1 className='Contact-title'>Contact</h1>
       <p className='Contact-info'>
-        If you have comments, questions or feedback about my work — just shoot me an email. For partnerships or
+        If you have any comments, questions or feedback about my work — just shoot me an email. For partnerships or
         collaborations inquiries please use email as well. Otherwise, feel free to reach out to me on social networks.
       </p>
-      <a className='Contact-link' href='mailto:alex_korzh@hey.com'>
-        alex_korzh@hey.com
+      <a className='Contact-link' href='mailto:alexkorzh7@pm.me'>
+        alexkorzh7@pm.me
       </a>
       <a className='Contact-link' href='https://github.com/AlexKLWS'>
         Github
       </a>
-      <a className='Contact-link' href='https://twitter.com/alexkorzh7'>
-        Twitter
-      </a>
       <a className='Contact-link' href='https://www.linkedin.com/in/aleksei-korzh-52952392'>
         LinkedIn
+      </a>
+      <div style={{ height: '16px' }} />
+      <span className='Twitter-caption'>My twitter about video games, code, art and everything related:</span>
+      <a className='Contact-link' href='https://twitter.com/longwinshadows'>
+        @longwinshadows
+      </a>
+      <span className='Twitter-caption'>My twitter about personal stuff, Russian politics, etc:</span>
+      <a className='Contact-link' href='https://twitter.com/alexkorzh7'>
+        @alexkorzh7
       </a>
       <div className='Bottom-section'>
         <div className='Contact-separator' />
