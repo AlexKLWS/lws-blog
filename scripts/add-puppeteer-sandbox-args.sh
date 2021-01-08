382dd63410981c71493d57e97225ffdfcb895365
Alex Korzh <alexkorzh7@gmail.com>
2021-01-09 01:21:39 +0300

    Remove macos implementation, since the fix is not needed on the platform

--- scripts/add-puppeteer-sandbox-args.sh
+++ scripts/add-puppeteer-sandbox-args.sh
@@ -1,9 +1,2 @@
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sed -i "s/puppeteerArgs: \[\],/puppeteerArgs: \[\"--no-sandbox\", \"--disable-setuid-sandbox\"\],/" ./node_modules/react-snap/index.js
-  echo changed arguments in react-snap
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  gsed -i "s/puppeteerArgs: \[\],/puppeteerArgs: \[\"--no-sandbox\", \"--disable-setuid-sandbox\"\],/" ./node_modules/react-snap/index.js
-  echo changed arguments in react-snap
-else
-  echo could not change arguments in react-snap
-fi
+sed -i "s/puppeteerArgs: \[\],/puppeteerArgs: \[\"--no-sandbox\", \"--disable-setuid-sandbox\"\],/" ./node_modules/react-snap/index.js
+echo changed arguments in react-snap
