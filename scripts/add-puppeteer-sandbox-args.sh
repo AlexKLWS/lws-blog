d63f8ec8cb43ef03594f7d3ea9d832218db7e0bf
Alex Korzh <alexkorzh7@gmail.com>
2021-01-15 17:10:03 +0300

    Make sure if sed fails, the entire script fails

--- scripts/add-puppeteer-sandbox-args.sh
+++ scripts/add-puppeteer-sandbox-args.sh
@@ -1,2 +1,2 @@
-sed -i "s/puppeteerArgs: \[\],/puppeteerArgs: \[\"--no-sandbox\", \"--disable-setuid-sandbox\"\],/" ./node_modules/react-snap/index.js
+sed -i "s/puppeteerArgs: \[\],/puppeteerArgs: \[\"--no-sandbox\", \"--disable-setuid-sandbox\"\],/" ./node_modules/react-snap/index.js && 
 echo changed arguments in react-snap
